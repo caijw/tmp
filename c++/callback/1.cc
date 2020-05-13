dd67b2998081eb27e637044a89f3bcd767361968
caijw <kingweicai@qq.com>
2020-05-13 11:14:34 +0800

    chore: add

--- c++/callback/1.cc
+++ c++/callback/1.cc
@@ -8,7 +8,8 @@ typedef std::function<void()> JsApiHandler;
 JsApiHandler jsApiHandler;
 class A {
 public:
-    A() {
+    A(int data_) {
+        data = data_;
         jsApiHandler = [this]() {
             std::cout << data << std::endl;
         };
@@ -19,8 +20,9 @@ public:
 int main() {
     // 出现悬挂引用， a已经销毁了
     {
-        A a;
+        A a(11);
     }
+    A a(1000);
     
     jsApiHandler();
 
